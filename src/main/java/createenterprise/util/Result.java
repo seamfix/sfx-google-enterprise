34fd2ce03c65316116e610aae67bc68492323ef4
jeffemuveyan <jemuveyan@seamfix.com>
2021-10-11 04:35:07 +0100

    Updated Result.java

--- src/main/java/createenterprise/util/Result.java
+++ src/main/java/createenterprise/util/Result.java
@@ -1,7 +1,40 @@
 package createenterprise.util;
 
 public class Result {
-    public Result (Type resultType, String signUpUrl, String enterpriseName) { }
+
+    private Type resultType;
+    private String signUpUrl;
+    private String enterpriseName;
+
+    public Result(Type resultType, String signUpUrl, String enterpriseName) {
+        this.resultType = resultType;
+        this.signUpUrl = signUpUrl;
+        this.enterpriseName = enterpriseName;
+    }
+
+    public Type getResultType() {
+        return resultType;
+    }
+
+    public void setResultType(Type resultType) {
+        this.resultType = resultType;
+    }
+
+    public String getSignUpUrl() {
+        return signUpUrl;
+    }
+
+    public void setSignUpUrl(String signUpUrl) {
+        this.signUpUrl = signUpUrl;
+    }
+
+    public String getEnterpriseName() {
+        return enterpriseName;
+    }
+
+    public void setEnterpriseName(String enterpriseName) {
+        this.enterpriseName = enterpriseName;
+    }
 }
 
 
